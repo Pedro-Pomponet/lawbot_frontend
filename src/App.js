ff28cdae36f7ccac1487b9768013408901c0fc51
Pedro-Pomponet <pomponetpedro15@gmail.com>
2025-04-17 18:35:26 -0300

    Fix: Restore lawyer dashboard routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/Landing/LandingPage';
+import LawyerLayout from './components/Lawyer/Layout/LawyerLayout';
+import LawyerDashboard from './components/Lawyer/Dashboard/LawyerDashboard';
+import CaseManagement from './components/Lawyer/CaseManagement/CaseManagement';
+import LegalLibrary from './components/Lawyer/Library/LegalLibrary';
+import Analytics from './components/Lawyer/Analytics/Analytics';
 import './App.css';
 
 function App() {
@@ -9,6 +14,12 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/advogado" element={<LawyerLayout />}>
+                    <Route index element={<LawyerDashboard />} />
+                    <Route path="casos" element={<CaseManagement />} />
+                    <Route path="biblioteca" element={<LegalLibrary />} />
+                    <Route path="analytics" element={<Analytics />} />
+                </Route>
             </Routes>
         </Router>
     );
